1d69f5cdda592b3fb757b3a33f06800947ac92f1
manjunatha-naidu <54593877+manjunatha-naidu@users.noreply.github.com>
2019-10-31 12:55:33 +0530

    Add files via upload

---
+++ Sell.php
@@ -0,0 +1,22 @@
+<form>
+  
+  <h2>b_nm</h2><input type="text"><br><br><br>
+  
+  <h2>b_subcat</h2><input type="text"><br><br>
+  
+  <h2>b_desc</h2><textarea rows="10" columns="5"></textarea><br><br>
+  
+  <h2>b_publisher</h2><input type="text"><br><br>
+  
+  <h2>b_edition</h2><input type="text"><br><br>
+  
+  <h2>b_isbn</h2><input type="text"><br><br>
+  
+  <h2>b_page</h2><input type="number"><br><br>
+  
+  <h2>b_price</h2><input type="number"><br><br>
+  
+  <h2>b_img</h2><input type="file"><br><br>
+  
+  <h2>b_pdf</h2><input type="file"><br><br>
+</form>
